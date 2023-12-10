9d6d9a0a759c1c5baeba1fc796e8b8281f4b8063
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2023-12-10 12:30:34 +0530

    Time: 4 ms (66.67%), Space: 17.7 MB (50.00%) - LeetHub

---
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countTestedDevices(vector<int>& batteryPercentages) {
+        int n=batteryPercentages.size();
+        int count=0;
+        for(int i=0; i<n; i++)
+        {
+            if(batteryPercentages[i]-count>0)
+            {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};
