56235e5c53486566bdc7526aaf9d1d3e3c5cb0fc
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2023-12-25 23:42:09 +0530

    Time: 4 ms (23.71%), Space: 6.7 MB (33.78%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,28 +1,15 @@
 class Solution {
 public:
-    int solve(int i, string s, vector<int> &dp, int n)
-    {
-        if(i== s.size() )
-            return 1;
-        if(s[i]=='0')
-            return 0;
-        
-        if(dp[i]!=-1)
-                return dp[i];
-        
-        int singlechar=0, bothchar=0;
-        singlechar+= solve(i+1, s, dp,n);
-        
-        if(i < n-1 && (s[i]=='1'|| (s[i]=='2'&& s[i+1]<'7')))
-            bothchar+=solve(i+2, s, dp, n); 
-        
-        return dp[i]= singlechar+bothchar;
-            
-    }
     int numDecodings(string s) {
-        int n= s.size();
-        vector<int> dp(n+1, -1);
-        
-        return solve(0, s, dp, n);
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            if (s[i] != '0') // Single digit
+                dp[i] += dp[i+1];
+            if (i+1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) // Two digits
+                dp[i] += dp[i+2];
+        }
+        return dp[0];
     }
 };
