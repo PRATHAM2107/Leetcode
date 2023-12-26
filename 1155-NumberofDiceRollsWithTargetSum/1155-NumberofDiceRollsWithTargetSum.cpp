ecea9d5438cf3f79660bfb867f3abe517c7173cb
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2023-12-26 20:48:37 +0530

    Update file 12/26/2023, 8:48:37 PM

--- 1155-NumberofDiceRollsWithTargetSum/1155-NumberofDiceRollsWithTargetSum.cpp
+++ 1155-NumberofDiceRollsWithTargetSum/1155-NumberofDiceRollsWithTargetSum.cpp
@@ -1,15 +1,10 @@
-        for (int t = 1; t <= target; ++t) {
-            for (int p = 1; p <= k; ++p) {
-                if (t >= p) {
-                    dp[i][t] = (dp[i][t] + dp[i - 1][t - p]) % MOD;
-                }
-            }
-        }
-    }
-
-    return dp[n][target];
-
-            
+    int numRollsToTarget(int d, int f, int target, int res = 0) {
+    if (d == 0 || target <= 0) return d == target;
+    if (dp[d][target]) return dp[d][target] - 1;
+    for (auto i = 1; i <= f; ++i)
+        res = (res + numRollsToTarget(d - 1, f, target - i)) % 1000000007;
+    dp[d][target] = res + 1;
+    return res;
     }
 };
 1
