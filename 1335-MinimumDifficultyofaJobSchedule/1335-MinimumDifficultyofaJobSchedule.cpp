3629ba4c157f5a5a4bea43d3a625abc44b69002a
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2023-12-29 23:51:32 +0530

    Create file

---
+++ 1335-MinimumDifficultyofaJobSchedule/1335-MinimumDifficultyofaJobSchedule.cpp
@@ -0,0 +1,10 @@
+        if(i==n && d==0) //end of our search with all elements covered and 0 days 
+remaining
+    int dfs(int i,int d,vector <int> &jd)
+    {
+    int dp[305][15];
+    int n;
+public:
+class Solution {
+#define INF 10000000
+[
