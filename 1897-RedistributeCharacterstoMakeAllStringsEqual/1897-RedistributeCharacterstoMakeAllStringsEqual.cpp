9fbdc8c215e4b107fcfae81410c19834ec99b312
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2023-12-30 23:16:09 +0530

    Create file

---
+++ 1897-RedistributeCharacterstoMakeAllStringsEqual/1897-RedistributeCharacterstoMakeAllStringsEqual.cpp
@@ -0,0 +1,10 @@
+        }
+            }
+                total+=1;
+                mp[c - 'a']++;
+            for (char c : s) {
+        for (string s : words) {
+        vector<int> mp(26, 0);
+        int total = 0;
+
+[
