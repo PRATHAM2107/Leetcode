ce8c32312b8012fdcb440192f7bdc0eade75fbe0
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-01-18 19:21:30 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int cur=1, pre=0, next;
+        while(n--)
+        {
+            next=cur+pre;
+            pre=cur;
+            cur=next;
+        }
+        return cur;
+    }
+};
