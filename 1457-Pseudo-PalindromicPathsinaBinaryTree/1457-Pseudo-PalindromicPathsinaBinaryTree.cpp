8409c9facc56acc95480f64a20100a5c6cfd27b0
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-01-24 22:21:06 +0530

    Create file

---
+++ 1457-Pseudo-PalindromicPathsinaBinaryTree/1457-Pseudo-PalindromicPathsinaBinaryTree.cpp
@@ -0,0 +1,10 @@
+                odd++;
+        if (!root->left && !root->right) {
+        int odd = 0;
+        for (int i=1; i<10; i++)
+            if (mp[i] %2 == 1)
+        {
+            return;
+        }
+        mp[root->val]++;
+[
