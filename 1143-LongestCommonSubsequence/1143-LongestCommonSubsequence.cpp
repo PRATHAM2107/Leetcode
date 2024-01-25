aebead7a7dfef39e991abe2d1fabf3c00037d83e
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-01-25 22:25:11 +0530

    Create file

---
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.cpp
@@ -0,0 +1,10 @@
+        if(dp[m][n]!=-1){
+            return dp[m][n];
+        }
+    int longestCommonSubsequence(string &text1, string &text2,int m,int n,int dp[][1001]) {
+        if(m==0 || n==0){
+            return 0;
+        }
+class Solution {
+public:
+"
