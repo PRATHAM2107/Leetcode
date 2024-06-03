34d5d63ebdd46e371f5004a5012dff126eb4978d
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-06-03 14:53:36 +0530

    Update file 6/3/2024, 2:53:59 PM

--- 206-ReverseLinkedList/206-ReverseLinkedList.cpp
+++ 206-ReverseLinkedList/206-ReverseLinkedList.cpp
@@ -1,11 +1,15 @@
-            cur->next=pre;
-            pre=cur;
-            cur=post;
-            post=post->next;
-        }
-        cur->next=pre;
-        return cur;
-
-    }
-};
+        // ListNode *pre, *cur, *post;
+        return h2;
+        // pre=NULL;
+        // cur=head;
+        // post= cur->next;
+        head->next=NULL;
+        head->next->next = head;
+        ListNode* h2 = reverseList(head->next);
+        // while(cur->next!= NULL)
+        // {
+        //     cur->next=pre;
+        //     pre=cur;
+        //     cur=post;
+        //     post=post->next;
 [
