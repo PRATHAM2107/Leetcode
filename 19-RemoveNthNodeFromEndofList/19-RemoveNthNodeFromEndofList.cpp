48996861ea7319effe6359c6b9b10f106fb57367
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-06-03 20:09:21 +0530

    Create file

---
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.cpp
@@ -0,0 +1,16 @@
+
+        for(int i = 1; i <= n; ++i)
+            fast = fast->next;
+    
+        while(fast->next != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        
+        slow->next = slow->next->next;
+        
+        return start->next; 
+    }
+};
+[1,2,3,4,5]
