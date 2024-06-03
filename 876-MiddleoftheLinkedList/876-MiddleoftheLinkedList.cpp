598cd6da611c4b029d63e9a1ca1f9987f5f182d9
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-06-03 14:36:38 +0530

    Create file

---
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+ */
+ * };
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode *next;
+ *     int val;
+ * struct ListNode {
+/**
+ * Definition for singly-linked list.
+    ListNode* middleNode(ListNode* head) {
+[
