5159db084e33d24cc9820dddbbd63afb45ae1e6e
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-06-03 12:32:11 +0530

    Create file

---
+++ 2486-AppendCharacterstoStringtoMakeSubsequence/2486-AppendCharacterstoStringtoMakeSubsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int i=0, j=0, n = s.size(), m= t.size();
+        while(j<n)
+        {
+            if(i<m && s[j]==t[i])
+            {
+                i++;
+            }
+           
+            j++;
+            
+        }
+        return m-i;
+    }
+};
+"coaching"
