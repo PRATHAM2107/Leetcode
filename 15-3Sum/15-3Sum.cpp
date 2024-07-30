d1170d078802b502a485b0a003d4b471102a190c
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-07-30 20:46:39 +0530

    Create file

---
+++ 15-3Sum/15-3Sum.cpp
@@ -0,0 +1,17 @@
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+public:
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+class Solution {
+[
+[-1,0,1,2,-1,-4]
+[0,1,1]
+[0,0,0]
+[[-1,-1,2],[-1,0,1]]
+[]
+[[0,0,0]]
+[[-1,-1,2],[-1,0,1]]
+[]
+[[0,0,0]]
