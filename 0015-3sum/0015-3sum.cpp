75ba8b09d3d052a224f10b20b289518912bb9d58
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-07-30 20:53:51 +0530

    Time: 59 ms (76.11%), Space: 27.4 MB (63.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -34,3 +34,29 @@ public:
         return result;
     }
 };
+
+    // vector<vector<int>> threeSum(vector<int>& nums) {
+    //     vector<vector<int>> result;
+    //     int n = nums.size();
+    
+    //     sort(nums.begin(), nums.end());
+
+    //     for (int i = 0; i < n - 2; ++i) {
+    //         if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+    //         unordered_set<int> seen;
+    //         for (int j = i + 1; j < n; ++j) {
+    //             int complement = -nums[i] - nums[j];
+                
+    //             if (seen.find(complement) != seen.end()) {
+    //                 result.push_back({nums[i], nums[j], complement});
+                    
+    //                 while (j + 1 < n && nums[j] == nums[j + 1]) ++j;
+    //             }
+                
+    //             seen.insert(nums[j]);
+    //         }
+    //     }
+        
+    //     return result;
+    // }
