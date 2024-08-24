d48d9502e9189ddd54eddc2e46f9221f6fa09cb5
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-08-24 21:39:50 +0530

    Create file

---
+++ 564-FindtheClosestPalindrome/564-FindtheClosestPalindrome.cpp
@@ -0,0 +1,16 @@
+        }
+        long mindiff = LONG_MAX;
+        long result;
+        long num = stol(n);
+        for(int i=0;i<5;i++)
+        {
+            if(candidates[i]!=num && abs(candidates[i]-num)<mindiff)
+            {
+                mindiff = abs(candidates[i]-num);
+"
+"123"
+"1"
+"121"
+"0"
+"121"
+"0"
