8bb63482775022773a09e58f3263116b13af8bab
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-09-01 02:52:25 +0530

    Time: 4 ms (70.36%), Space: 12.6 MB (6.76%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -15,7 +15,7 @@ public:
         vector<int> ans;
         for(int i=0; i<nums1.size(); i++)
         {
-            if(mp.find(nums1[i])==mp.end())
+            if(!mp[nums1[i]])
                 ans.push_back(-1);
             else
                 ans.push_back(mp[nums1[i]]);
