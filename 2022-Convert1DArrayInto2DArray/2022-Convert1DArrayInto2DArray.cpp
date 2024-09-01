deceec2026fb17f69d52ad5424da0854064682ea
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-09-01 20:25:54 +0530

    Create file

---
+++ 2022-Convert1DArrayInto2DArray/2022-Convert1DArrayInto2DArray.cpp
@@ -0,0 +1,17 @@
+        for(int i=0; i<m && k<sz; i++)
+        {
+            for(int j=0; j<n && k<sz ; j++)
+            {
+                ans[i][j]= original[k++];
+            }
+        int k=0, sz=original.size();
+        vector<vector<int>> ans(m, vector<int>(n, 0));
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+public:
+class Solution {
+        if(m*n!=sz)  
+            return {};
+[1,2,3,4]
+[
+[
+[
