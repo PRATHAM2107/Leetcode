e0bbc7d6b99251d0b88d006fa0dd354f753c0c42
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-09-01 20:30:59 +0530

    Create file

---
+++ 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.cpp
@@ -0,0 +1,14 @@
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* cur) {
+         vector < int > postOrder;
+    if (cur == NULL) return postOrder;
+
+    stack < TreeNode * > st;
+[1,null,2,3]
