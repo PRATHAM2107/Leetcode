2cbf58907d2ce6d1ece028ffe9c119c641412dd0
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-09-02 14:31:47 +0530

    Create file

---
+++ 42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -0,0 +1,19 @@
+        int ans = 0;
+        
+            rightMax[i] = max(height[i+1], rightMax[i+1]);
+        for (int i = n-2; i >= 0; --i) 
+        for (int i = 1; i < n; ++i) 
+            leftMax[i] = max(height[i-1], leftMax[i-1]);
+        vector<int> leftMax(n), rightMax(n);
+        int n = height.size();
+    int trap(vector<int>& height) {
+public:
+class Solution {
+
+[
+[0,1,0,2,1,0,1,3,2,1,2,1]
+[4,2,0,3,2,5]
+6
+9
+6
+9
