3eda093b4efec16aa9b23c2e77ee0dd0ffa65438
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-09-04 01:29:10 +0530

    Create file

---
+++ 1945-SumofDigitsofStringAfterConvert/1945-SumofDigitsofStringAfterConvert.cpp
@@ -0,0 +1,26 @@
+        }
+            ans+= to_string(p);
+        while(k--)
+        {
+            n=0;
+            for(char c: ans)
+                n+=(c-'0');
+            ans= to_string(n);
+            int p= c-'a'+1;
+        {
+        }
+
+        for(char c: s)
+"
+"iiii"
+1
+"leetcode"
+2
+"zbax"
+2
+36
+6
+8
+36
+6
+8
