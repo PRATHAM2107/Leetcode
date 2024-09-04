aba363786ec14a7ed2a3995230c6704e14c823c0
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-09-04 12:15:09 +0530

    Create file

---
+++ 874-WalkingRobotSimulation/874-WalkingRobotSimulation.cpp
@@ -0,0 +1,29 @@
+        int d = 0; 
+        int dist = 0;
+
+            st.insert({x[0], x[1]});
+        }
+
+        int x = 0, y = 0;
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+set<pair<int, int>> st;
+        for(auto x : obstacles) {
+[
+[4,-1,3]
+[]
+[4,-1,4,-2,4]
+[[2,4]]
+[6,-1,-1,6]
+[]
+[7,-2,-2,7,5]
+[[-3,2],[-2,1],[0,1],[-2,4],[-1,0],[-2,-3],[0,-3],[4,4],[-3,3],[2,2]]
+25
+65
+36
+4
+25
+65
+36
+4
