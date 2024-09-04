fdd27b9b5086d57d053ce7002ce0c5aa23f046cf
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-09-04 12:15:08 +0530

    Time: 70 ms (71.27%), Space: 39.6 MB (59.67%) - LeetHub

---
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+set<pair<int, int>> st;
+        for(auto x : obstacles) {
+            st.insert({x[0], x[1]});
+        }
+
+        int x = 0, y = 0;
+        int d = 0; 
+        int dist = 0;
+
+        vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        for(int i : commands) {
+            if(i == -1) {  
+                d = (d + 1) % 4;
+            } else if(i == -2) {  
+                d = (d + 3) % 4;
+            } else {
+                for(int j = 0; j < i; j++) {
+                    int new_x = x + directions[d].first;
+                    int new_y = y + directions[d].second;
+                    if(st.find({new_x, new_y}) != st.end()) {
+                        break;
+                    }
+                    x = new_x;
+                    y = new_y;
+                    dist = max(dist, x * x + y * y);
+                }
+            }
+        }
+        
+        return dist;
+    }
+};
