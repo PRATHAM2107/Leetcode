4796b2a138a2dfa6242848cd2f26c8e69b1f24e0
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-10-07 00:09:00 +0530

    Create file

---
+++ 1813-SentenceSimilarityIII/1813-SentenceSimilarityIII.cpp
@@ -0,0 +1,22 @@
+            }
+            else if(s1[end1]==s2[end2]) //if end of both vectors are equal
+            {
+                end1--; end2--;
+            }
+            else  return false;
+        }
+        return true;
+    }
+"
+"My name is Haley"
+"My Haley"
+"of"
+"A lot of words"
+"Eating right now"
+"Eating"
+true
+false
+true
+true
+false
+true
