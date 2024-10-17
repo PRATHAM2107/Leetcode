292b2f278b79faf8f1d2e33db58d27e0f7c7d6e9
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-10-17 22:08:47 +0530

    Create file

---
+++ 670-MaximumSwap/670-MaximumSwap.cpp
@@ -0,0 +1,21 @@
+                leftidx = i;
+                rightidx = maxidx;
+                maxidx = i;
+            }
+            else if(temp[i] < maxdigit) {
+            
+            if (temp[i] > maxdigit) {
+                maxdigit = temp[i];
+2
+2736
+9973
+98368
+1993
+7236
+9973
+98863
+9913
+7236
+9973
+98863
+9913
