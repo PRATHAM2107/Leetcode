e3345deeccebda0eae71300f518b2b9df7317b6f
PRATHAM2107 <96982596+PRATHAM2107@users.noreply.github.com>
2024-10-23 23:30:14 +0530

    Update file 10/23/2024, 11:30:14 PM

--- 2641-CousinsinBinaryTreeII/2641-CousinsinBinaryTreeII.cpp
+++ 2641-CousinsinBinaryTreeII/2641-CousinsinBinaryTreeII.cpp
@@ -1,1 +1,17 @@
-            dfs2(root->right, lvl + 1, sums, leftbro); 
+    void dfs2(TreeNode* root, int lvl, vector<int> &sums, int bro) {
+    }
+
+            sums[lvl] += root->val; 
+        dfs(root->left, lvl + 1, sums); 
+        dfs(root->right, lvl + 1, sums); 
+        if (!root) return;
+
+        root->val = sums[lvl] - (root->val + bro); 
+
+        int rightbro = root->right ? root->right->val : 0;
+        int leftbro = root->left ? root->left->val : 0; 
+
+            
+        if (root->left) 
+            dfs2(root->left, lvl + 1, sums, rightbro); 
+        
